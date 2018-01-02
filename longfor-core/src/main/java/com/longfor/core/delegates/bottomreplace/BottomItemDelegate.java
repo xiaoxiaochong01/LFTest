53123e027280f3063cf0c98401d79e238061658f
tong zhen hua <tongzhenhua1120@rayootech.com>
2018-01-02 13:59:09 +0800

    返回bug修复

--- longfor-core/src/main/java/com/longfor/core/delegates/bottomreplace/BottomItemDelegate.java
+++ longfor-core/src/main/java/com/longfor/core/delegates/bottomreplace/BottomItemDelegate.java
@@ -1,5 +1,6 @@
 package com.longfor.core.delegates.bottomreplace;
 
+import android.content.Context;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -14,17 +15,19 @@ public abstract class BottomItemDelegate extends LongForDelegate implements View
     //连续点击两次退出程序时间设置
     private static final long WATE_TIME = 2000L;
     private long touch_time = 0;
+    private BackHandledInterface mBackHandledInterface;
 
-//    @Override
-//    public boolean onBackPressedSupport() {
-//        if(System.currentTimeMillis() - touch_time < WATE_TIME){
-//            _mActivity.finish();
-//        }else{
-//            touch_time = System.currentTimeMillis();
-//            Toast.makeText(_mActivity, "双击退出", Toast.LENGTH_SHORT).show();
-//        }
-//        return true;
-//    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (!(getActivity() instanceof BackHandledInterface)) {
+            throw new ClassCastException(
+                    "Hosting Activity must implement BackHandledInterface");
+        } else {
+            this.mBackHandledInterface = (BackHandledInterface) getActivity();
+        }
+    }
 
     @Override
     public void onResume() {
@@ -38,6 +41,27 @@ public abstract class BottomItemDelegate extends LongForDelegate implements View
     }
 
     @Override
+    public void onStart() {
+        mBackHandledInterface.setSelectedFragment(this);
+        super.onStart();
+    }
+
+
+    public boolean onBackPressed() {
+        if(System.currentTimeMillis() - touch_time < WATE_TIME){
+            _mActivity.finish();
+            if(touch_time!=0){
+                touch_time = 0;
+            }
+            return true;
+        }else{
+            touch_time = System.currentTimeMillis();
+            Toast.makeText(_mActivity, "双击退出", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
     public boolean onKey(View view, int i, KeyEvent keyEvent) {
         if(System.currentTimeMillis() - touch_time < WATE_TIME){
             _mActivity.finish();
@@ -45,6 +69,17 @@ public abstract class BottomItemDelegate extends LongForDelegate implements View
                 touch_time = 0;
             }
             return true;
+        }else{
+            touch_time = System.currentTimeMillis();
+            Toast.makeText(_mActivity, "双击退出", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onBackPressedSupport() {
+        if(System.currentTimeMillis() - touch_time < WATE_TIME){
+            _mActivity.finish();
         }else{
             touch_time = System.currentTimeMillis();
             Toast.makeText(_mActivity, "双击退出", Toast.LENGTH_SHORT).show();
