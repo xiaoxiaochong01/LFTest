b5401ecc6e56200b49fb8dc7bb8aae5fc7a1225d
tong zhen hua <tongzhenhua1120@rayootech.com>
2018-01-04 16:08:16 +0800

    首页布局

--- channelmanager/src/main/java/com/longfor/channelmanager/mine/delegate/RecommendDelegate.java
+++ channelmanager/src/main/java/com/longfor/channelmanager/mine/delegate/RecommendDelegate.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.longfor.channelmanager.R;
 import com.longfor.channelmanager.R2;
 import com.longfor.channelmanager.common.constants.Constant;
 import com.longfor.channelmanager.common.view.commonheadview.CommonHeadView;
+import com.longfor.channelmanager.mine.constants.ConstantMine;
 import com.longfor.core.delegates.LongForDelegate;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * @author: tongzhenhua
@@ -22,6 +28,10 @@ import butterknife.BindView;
 public class RecommendDelegate extends LongForDelegate {
     @BindView(R2.id.recommend_head)
     CommonHeadView mHeadView;
+    @BindView(R2.id.tv_recommend_code)
+    TextView tvRecommendCode;
+
+
 
     @Override
     public Object setLayout() {
@@ -32,11 +42,15 @@ public class RecommendDelegate extends LongForDelegate {
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
 
         Bundle bundle = getArguments();
-        if(bundle != null) {
+        if (bundle != null) {
             String leftText = bundle.getString(Constant.TITLE_LEFT_TEXT);
             if (!TextUtils.isEmpty(leftText)) {
                 mHeadView.setLeftMsgVisiable(true);
                 mHeadView.setLeftMsg(leftText);
+            }
+            String recommendCode = bundle.getString(ConstantMine.RECOMMEND_CODE);
+            if(!TextUtils.isEmpty(recommendCode)) {
+                tvRecommendCode.setText(recommendCode);
             }
         }
         mHeadView.setLeftBackImageVisible(true);
@@ -48,4 +62,5 @@ public class RecommendDelegate extends LongForDelegate {
             }
         });
     }
+
 }
