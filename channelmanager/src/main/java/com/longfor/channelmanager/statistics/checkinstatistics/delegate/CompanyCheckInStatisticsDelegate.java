90d76ac6a7dab59c279af04bf82a43632e2b022e
gaomei <wushuang0012@gmail.com>
2018-01-11 18:02:01 +0800

    考勤查询切换项目刷新数据

---
+++ channelmanager/src/main/java/com/longfor/channelmanager/statistics/checkinstatistics/delegate/CompanyCheckInStatisticsDelegate.java
@@ -0,0 +1,26 @@
+package com.longfor.channelmanager.statistics.checkinstatistics.delegate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.longfor.core.delegates.LongForDelegate;
+
+/**
+ * @author: gaomei
+ * @date: 2018/1/11
+ * @function:地区级别上岗统计
+ */
+
+public class CompanyCheckInStatisticsDelegate extends LongForDelegate {
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
