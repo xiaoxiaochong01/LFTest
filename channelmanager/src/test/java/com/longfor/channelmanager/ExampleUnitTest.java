81140b870c584696f148d29a27849488ac5057f0
gaomei <wushuang0012@gmail.com>
2018-01-29 10:38:45 +0800

    test

--- channelmanager/src/test/java/com/longfor/channelmanager/ExampleUnitTest.java
+++ channelmanager/src/test/java/com/longfor/channelmanager/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package com.longfor.channelmanager;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void parseStr2Date(){
+//        2018-01-29
+        try {
+//            String string = "2016-10-24 21:59:06";
+            // TODO: 2018/1/29 test
+            String string = "2018-01-29";
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            System.out.println(sdf.parse(string));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
